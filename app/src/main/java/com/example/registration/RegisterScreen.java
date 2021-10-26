3ed78a4643aef5499eaab7a4843fb84a6a1f500c
Stephen Ivan Llorente <ivanllorente0318@gmail.com>
2021-10-26 13:22:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/registration/RegisterScreen.java
@@ -0,0 +1,16 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
