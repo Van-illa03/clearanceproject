eff4dcb46dbc9482dbf98c4529a9c6fb387d77e2
Stephen Ivan Llorente <ivanllorente0318@gmail.com>
2021-10-26 15:23:47 +0800

    Login and Register form are designed first without the code

--- app/src/main/java/com/example/registration/RegisterScreen.java
+++ app/src/main/java/com/example/registration/RegisterScreen.java
@@ -3,8 +3,19 @@ package com.example.registration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterScreen extends AppCompatActivity {
+    EditText nameStaff,usernameStaff,emailStaff,passwordStaff,passwordStaff2;
+    Button registerButton;
+    TextView alreadyRegisteredButton;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
 
 
@@ -12,5 +23,19 @@ public class RegisterScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+
+        nameStaff       =   findViewById(R.id.nameStaff);
+        usernameStaff   =   findViewById(R.id.usernameStaff);
+        emailStaff      =   findViewById(R.id.emailStaff);
+        passwordStaff   =   findViewById(R.id.passwordStaff);
+        passwordStaff2  =   findViewById(R.id.passwordStaff2);
+        registerButton  =   findViewById(R.id.registerButton);
+        alreadyRegisteredButton =   findViewById(R.id.alreadyRegistered);
+        fAuth   =   FirebaseAuth.getInstance();
+        progressBar     =   findViewById(R.id.progressBar);
+
+
+
+
     }
 }
