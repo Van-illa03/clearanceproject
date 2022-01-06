0a3dc4f1764a7b8393e865ffbfdffcfef00faa77
Van-illa03 <ivanllorente0318@gmail.com>
2022-01-06 12:07:47 +0800

    Fixed the uploading of photos in storage. Uri can now be stored in firestore. Needs to fix the appropriate detail about the data.

--- app/src/main/java/cvsu/clearance/app/StaffProfile.java
+++ app/src/main/java/cvsu/clearance/app/StaffProfile.java
@@ -1,23 +1,37 @@
 package cvsu.clearance.app;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StaffProfile extends AppCompatActivity {
 
@@ -25,6 +39,7 @@ public class StaffProfile extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
+    private FirebaseFirestore mStore;
     private Button saveBtn, chooseBtn;
     private ImageView signatureResult;
     private ProgressBar progressBar;
@@ -45,8 +60,8 @@ public class StaffProfile extends AppCompatActivity {
         chooseBtn = findViewById(R.id.chooseBtn);
         signatureResult = findViewById(R.id.signatureResult);
         progressBar = findViewById(R.id.progressBar);
-
-
+        mStorageRef = FirebaseStorage.getInstance().getReference("signatures");
+        mStore  =   FirebaseFirestore.getInstance();
 
         chooseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,8 +114,86 @@ public class StaffProfile extends AppCompatActivity {
 
     }
 
-    private void uploadFile() {
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
 
+    private void uploadFile() {
+        if (mImageUri != null) {
+            // Temporary named in time. To be changed as Signing Station name in the future
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(StaffProfile.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Map<String,Object> staffSignature = new HashMap<>();
+                                    FirebaseUser User = mAuth.getCurrentUser();
+                                    // Temporarily named "Signing Station" which is to be changed after updating the Firestore
+                                    Upload upload = new Upload("SigningStation",
+                                            uri.toString());
+                                    // The value of uploadId is to be changed to
+                                    String uploadId = User.getUid();
+
+                                    staffSignature.put("Staff ID",User.getUid());
+                                    staffSignature.put("SignatureURI",upload);
+
+                                    // (Reference from tutorial) ->mDatabaseRef.child(uploadId).setValue(upload);
+
+                                    // Storing the information of user
+
+                                    mStore.collection("Signatures").document(uploadId).set(staffSignature)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d("","DocumentSnapshot successfully written!");
+
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("", "Error in DocumentSnapshot!");
+                                        }
+                                    });
+
+                                }
+                            });
+
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(StaffProfile.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressBar.setProgress((int) progress);
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
