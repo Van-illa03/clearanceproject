4767e9f8b52884cf32798854be7fec423db71e3c
jemrel mangaliman <jemrelricky.mangaliman@cvsu.edu.ph>
2022-01-07 17:44:10 +0800

    designed the front, login, and registration pages

--- app/src/main/java/cvsu/clearance/app/FrontScreen.java
+++ app/src/main/java/cvsu/clearance/app/FrontScreen.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class FrontScreen extends AppCompatActivity {
 
     Button buttonStart;
+    TextView Appname;
+    ImageView Logo;
+    Animation logoanim, titleanim, buttonanim;
 
 
     @Override
@@ -20,7 +26,15 @@ public class FrontScreen extends AppCompatActivity {
         setContentView(R.layout.activity_front_screen);
 
         buttonStart =   findViewById(R.id.buttonStart);
+        Appname = findViewById(R.id.AppName);
+        Logo = findViewById(R.id.AppLogo);
+        logoanim = AnimationUtils.loadAnimation(this, R.anim.logo_anim);
+        titleanim = AnimationUtils.loadAnimation(this, R.anim.title_anim);
+        buttonanim = AnimationUtils.loadAnimation(this, R.anim.button_anim);
 
+        buttonStart.setAnimation(buttonanim);
+        Appname.setAnimation(titleanim);
+        Logo.setAnimation(logoanim);
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
