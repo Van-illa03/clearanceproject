535a8e7998548e4f0515710ee9bdeca93bdc98f0
jemrel mangaliman <jemrelricky.mangaliman@cvsu.edu.ph>
2022-06-04 13:46:41 +0800

    signatures now appear based on complete/incomplete requirements status modified the pop up content when the signature slot was clicked

--- app/src/main/java/cvsu/clearance/app/Adapter.java
+++ app/src/main/java/cvsu/clearance/app/Adapter.java
@@ -7,16 +7,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -44,7 +49,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     FirebaseFirestore mStore;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-    TextView SlotViewStationName, SlotViewStationLocation;
+    TextView SlotViewStationName, SlotViewStationLocation, SlotViewRequirementDescription;
+    Spinner RequirementsSpinner;
+    String [] Requirements;
+    String CurrentRequirement;
+    int [] firstcounter = new int[1];
+    int secondcounter = 0;
+
 
     public Adapter (Context ctx, List<String> stationNames, ArrayList<String> signatures){
         this.StationNames = stationNames;
@@ -105,9 +116,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
                     holder.getBindingAdapterPosition();
                     dialogBuilder = new AlertDialog.Builder(context);
                     final View DialogView = inflater.inflate(R.layout.showstationdetails,null);
+                    firstcounter[0] = 0;
+                    secondcounter = 0;
+
 
                     SlotViewStationName = (TextView) DialogView.findViewById(R.id.SignSlotStationNameText);
                     SlotViewStationLocation = (TextView) DialogView.findViewById((R.id.SignSlotStationLocationText));
+                    SlotViewRequirementDescription = (TextView) DialogView.findViewById(R.id.SignSlotRequirementDescriptionText);
+                    RequirementsSpinner = (Spinner) DialogView.findViewById(R.id.SignSlotRequirementsSpinner);
 
 
                     mStore.collection("SigningStation").document(StationNames.get(position)).get()
@@ -117,6 +133,91 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
                                     CatchStationDetails catchStationDetails = documentSnapshot.toObject(CatchStationDetails.class);
                                     SlotViewStationName.setText(catchStationDetails.getSigning_Station_Name());
                                     SlotViewStationLocation.setText(catchStationDetails.getLocation());
+
+                                }
+                            });
+
+                    //listener whenever there is an item change in spinner
+                    RequirementsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                            CurrentRequirement = Requirements[pos];
+
+                            //gets the requirement name of the specific signing station
+                            mStore.collection("SigningStation").document(StationNames.get(position)).collection("Requirements").document(CurrentRequirement).get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()){
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document.exists()){
+
+                                                    String RequirementsDescriptionCatch = document.getString("Description");
+                                                    SlotViewRequirementDescription.setText(RequirementsDescriptionCatch);
+
+                                                }
+                                            }
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+                    //getting requirement count of the signing station
+                    mStore.collection("SigningStation").document(StationNames.get(position)).collection("Requirements").get()
+                            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                                    for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                        CatchRequirementsDetails catchRequirementsDetails = documentSnapshot.toObject(CatchRequirementsDetails.class);
+
+                                        String RequirementsNameCatch = catchRequirementsDetails.getRequirementsName();
+                                        if (RequirementsNameCatch != null) {
+                                            firstcounter[0] = firstcounter[0] + 1;
+                                        }
+                                    }
+                                }
+                            });
+
+
+                    //inserting the requirements name into the Requirements array, then passing it in the array adapter
+                    mStore.collection("SigningStation").document(StationNames.get(position)).collection("Requirements").get()
+                            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                    Requirements = new String [firstcounter[0]];
+
+                                    for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                        CatchRequirementsDetails catchRequirementsDetails = documentSnapshot.toObject(CatchRequirementsDetails.class);
+
+                                        String RequirementsNameCatch = catchRequirementsDetails.getRequirementsName();
+                                        if (RequirementsNameCatch != null) {
+                                            Requirements[secondcounter] = RequirementsNameCatch;
+                                            secondcounter++;
+                                        }
+                                    }
+                                    if (Requirements.length == 0){
+                                        Requirements = new String [1];
+                                        Requirements[0] = "None";
+                                        Log.d("Req Length is 1: ", "Requirements content:" + Requirements[0]);
+                                        ArrayAdapter AA = new ArrayAdapter (context, android.R.layout.simple_spinner_item, Requirements);
+                                        AA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                        //Setting the ArrayAdapter data on the Spinner
+                                        RequirementsSpinner.setAdapter(AA);
+                                    }
+                                    else {
+                                        Log.d("Req Length is " + Requirements.length +": ", "Requirements content:" + Requirements[0]);
+                                        ArrayAdapter AA = new ArrayAdapter (context, android.R.layout.simple_spinner_item, Requirements);
+                                        AA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                        //Setting the ArrayAdapter data on the Spinner
+                                        RequirementsSpinner.setAdapter(AA);
+                                    }
 
                                 }
                             });
